5a01dab3ec2f0051253073f4253a2d6cd8d1c810
KR-ISCODE <krisjenxz@gmail.com>
2026-02-22 15:50:47 +0700

    first commit

---
+++ final-project/Controllers/HealthController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using final_project.Data;
+
+namespace final_project.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public HealthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("db")]
+        public async Task<IActionResult> TestDatabaseConnection()
+        {
+            try
+            {
+                var canConnect = await _context.Database.CanConnectAsync();
+
+                if (canConnect)
+                {
+                    return Ok(new
+                    {
+                        status = "Success",
+                        message = "Database connection successful ✅"
+                    });
+                }
+
+                return StatusCode(500, new
+                {
+                    status = "Failed",
+                    message = "Cannot connect to database ❌"
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    status = "Error",
+                    message = ex.Message
+                });
+            }
+        }
+    }
+}
---
+++ final-project/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace final_project.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Put table here from model
+        //public DbSet<TestUser> TestUsers { get; set; }
+    }
+}
