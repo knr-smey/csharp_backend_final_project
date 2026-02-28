06e01405ef9c2bd0a7ff06a2505c9a4aee3dcc49
KR-ISCODE <krisjenxz@gmail.com>
2026-02-28 16:55:55 +0700

    User Controller Code test

--- final-project/Controllers/UserController.cs
+++ final-project/Controllers/UserController.cs
@@ -7,5 +7,10 @@ namespace final_project.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello, User!");
+        }
     }
 }
