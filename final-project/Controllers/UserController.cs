057c67d467f9aa6573bc9cdef427237c632cdd23
KR-ISCODE <krisjenxz@gmail.com>
2026-02-28 16:25:32 +0700

    Remove HealtheController

---
+++ final-project/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace final_project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
--- final-project/obj/Debug/net8.0/final-project.AssemblyInfo.cs
+++ final-project/obj/Debug/net8.0/final-project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("final-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+294f2ec05b832b25bba98ce069b4c300b438f0f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f57cb7d2a29c71877e43cdfeffcee6cce6130452")]
 [assembly: System.Reflection.AssemblyProductAttribute("final-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("final-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
