5219220dd181e0de03c6cee8acb14a56afddc6a9
yonglyhor1004-create <yonglyhor1004@gmail.com>
2026-03-01 17:06:22 +0700

    Task: Feature/Login to system

---
+++ final-project/DTOs/Auth/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace final_project.DTOs.Auth
+{
+    public class LoginRequestDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ final-project/DTOs/Auth/LoginResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace final_project.DTOs.Auth
+{
+    public class LoginResponseDto
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string TokenType { get; set; } = "Bearer";
+        public int ExpiresIn { get; set; } // seconds
+        public object User { get; set; } = new { };
+    }
+}
